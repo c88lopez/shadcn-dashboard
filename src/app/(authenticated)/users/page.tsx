c097395909376cb27e7a52adffcdabc14fb4b4bc
Cristian Lopez <cristian@getrentcheck.com>
2025-03-12 16:25:15 -0300

    simplify

--- src/app/(authenticated)/users/page.tsx
+++ src/app/(authenticated)/users/page.tsx
@@ -3,12 +3,11 @@ import { auth } from "@/auth";
 
 export default async function Users() {
   const session = await auth();
-  const graphqlServerUrl = process.env.GRAPHQL_SERVER;
 
   return (
     <div className="container mx-auto py-10">
       <DataTable
-        graphqlServerUrl={graphqlServerUrl}
+        graphqlServerUrl={process.env.GRAPHQL_SERVER}
         accessToken={session?.user?.id}
       />
     </div>
