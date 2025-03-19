8234b6c4296166720353fb7a6341e31e75f2cb06
Cristian Lopez <cristian@getrentcheck.com>
2025-03-19 14:25:03 -0300

    example for the main page

--- src/app/(authenticated)/layout.tsx
+++ src/app/(authenticated)/layout.tsx
@@ -32,7 +32,7 @@ export default async function Layout({
           </div>
         </header>
 
-        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">{children}</div>
+        <div className="flex flex-1 flex-col">{children}</div>
 
         <Toaster />
       </SidebarInset>
